67481ba1dd932910ec3759495a2af48d81944c7c
Avi parashar <avipara62@gmail.com>
2026-07-18 22:31:34 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (76.18%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n = bills.size();
+        int cnt5 = 0;
+        int cnt10 = 0;
+        for(int i=0;i<n;i++){
+            if(bills[i]==5){
+                cnt5++;
+            }
+            else if(bills[i]==10){
+                if(cnt5) {
+                    cnt10++;
+                    cnt5--;
+                }else{
+                    return false;
+                }
+            }
+            else{
+                if(cnt10 && cnt5){
+                    cnt10--;
+                    cnt5--;
+                }else if(cnt5 >= 3){
+                    cnt5-=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+
+    }
+};
