62db92c37cc5c62063ac4c681d8774e59de5414b
Avi parashar <avipara62@gmail.com>
2026-07-18 12:17:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (32.39%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int n = str1.length();
+        int m = str2.length();
+         if(str1+str2 != str2+str1) return "";
+
+        int gcdi = gcd(n,m);
+        return str1.substr(0,gcdi);
+    }
+};
