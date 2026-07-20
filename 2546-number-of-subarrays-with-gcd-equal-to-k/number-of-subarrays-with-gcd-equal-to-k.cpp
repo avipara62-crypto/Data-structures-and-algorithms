fe886f36bd8aaa16efeb6386afd1e3f17582309b
Avi parashar <avipara62@gmail.com>
2026-07-21 05:07:08 +0530

    Time: 95 ms (13.68%) | Memory: 12.5 MB (99.33%) - LeetSync

---
+++ 2546-number-of-subarrays-with-gcd-equal-to-k/number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarrayGCD(vector<int>& nums, int k) {
+        int n = nums.size();
+        int cnt =0;
+        for(int i=0;i<n;i++){
+            int g = 0;
+            for(int j=i;j<n;j++){
+                g = gcd(g,nums[j]);
+                if(g==k) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
