5878ea079169d7eb4b8fcd7f16a048056f8df729
Avi parashar <avipara62@gmail.com>
2026-07-21 02:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (99.68%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,22 +1,46 @@
+// class Solution {
+// public:
+//     int longestPalindrome(string s) {
+//         int n = s.length();
+//         unordered_map<char,int> mp;
+//         for(char c : s){
+//             mp[c]++;
+//         }
+//         int ans = 0;
+//         bool odd = false;
+//         for(auto &it : mp){
+//             if(it.second % 2 == 0){
+//                 ans += it.second;
+//             }
+//             else{
+//                 ans += it.second-1;
+//                 odd = true;
+//             }
+//         }
+//         return odd ? ans+1 : ans;
+//     }
+// };
+
+// OPTIMISATION 
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int n = s.length();
-        unordered_map<char,int> mp;
+        int freq[128] ={0};
         for(char c : s){
-            mp[c]++;
+            freq[c]++;
         }
         int ans = 0;
         bool odd = false;
-        for(auto &it : mp){
-            if(it.second % 2 == 0){
-                ans += it.second;
+        for(int i=0;i<128;i++){
+            if(freq[i] % 2 == 0){
+                ans += freq[i];
             }
             else{
-                ans += it.second-1;
-                odd = true;
+                ans += freq[i]-1;
+                odd =true;
             }
         }
-        return odd ? ans+1 : ans;
+        if(odd) ans ++;
+        return ans;
     }
 };
