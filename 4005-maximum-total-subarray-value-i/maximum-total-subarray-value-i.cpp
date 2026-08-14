7ea077d4758f67b018ee9f87f03b92e2492c1129
Avi parashar <avipara62@gmail.com>
2026-08-14 15:29:49 +0530

    Time: 7 ms (11.65%) | Memory: 103.7 MB (36.88%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        long long ans = 0;
+        long long maxi = *max_element(nums.begin(),nums.end());
+        long long mini = *min_element(nums.begin(),nums.end());
+        while(k--){
+            ans+=(maxi-mini);
+        }
+        return ans;
+    }
+};
