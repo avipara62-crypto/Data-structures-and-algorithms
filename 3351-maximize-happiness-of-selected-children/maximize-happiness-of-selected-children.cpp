d637af7618b1ff141602960c206abe2d0c4e96cc
Avi parashar <avipara62@gmail.com>
2026-08-22 17:05:58 +0530

    Time: 205 ms (18.20%) | Memory: 117.3 MB (8.40%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,36 +1,36 @@
-// class Solution {
-// public:
-//     long long maximumHappinessSum(vector<int>& happiness, int k) {
-//         int n = happiness.size();
-//         priority_queue<int> pq;
-//         for(int x : happiness){
-//             pq.push(x);
-//         }
-//         long long ans = 0;
-//         int count =0;
-//         while(k--){
-//             int x = pq.top();
-//             pq.pop();
-//             ans+=max(x-count,0);
-//             count++;
-//         }
-//         return ans;
-//     }
-// };
-
-
-
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         int n = happiness.size();
-        sort(happiness.rbegin(),happiness.rend());
-        int count = 0;
+        priority_queue<int> pq;
+        for(int x : happiness){
+            pq.push(x);
+        }
         long long ans = 0;
-        for(int i=0;i<k;i++){
-            ans += max(happiness[i]-count,0);
+        int count =0;
+        while(k--){
+            int x = pq.top();
+            pq.pop();
+            ans+=max(x-count,0);
             count++;
         }
         return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     long long maximumHappinessSum(vector<int>& happiness, int k) {
+//         int n = happiness.size();
+//         sort(happiness.rbegin(),happiness.rend());
+//         int count = 0;
+//         long long ans = 0;
+//         for(int i=0;i<k;i++){
+//             ans += max(happiness[i]-count,0);
+//             count++;
+//         }
+//         return ans;
+//     }
+// };
