14f5ee9e5d59ce21b31fb8699e25c3f1471ddb69
Avi parashar <avipara62@gmail.com>
2026-08-24 02:13:13 +0530

    Time: 3 ms (89.06%) | Memory: 71.4 MB (95.08%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int sum(vector<int>& nums,int k){
+        if(k<0) return 0;
+        int l=0,r=0,sum=0,cnt=0;
+        while(r<nums.size()){
+            sum+=(nums[r]%2);
+            while(sum>k){
+                sum-=(nums[l]%2);
+                l++;
+            }
+            cnt+=(r-l+1);
+            r++;
+        }
+        return cnt;
+    }
+ 
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return sum(nums,k)-sum(nums,k-1);
+    }
+};
